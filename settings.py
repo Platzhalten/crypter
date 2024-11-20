e7e7ca0ca5e60b7967cd9cce5b479fdd87f2a21b
eric <accountemail@tutanota.com>
2024-11-20 11:05:09 +0100

    added settings files

--- settings.py
+++ settings.py
@@ -1,1 +1,15 @@
+import json
 
+
+def get_settings(what: list = None) -> dict:
+    with open("settings/settings.json", "r") as f:
+        setting = json.load(f)
+
+    if what:
+
+        for i in what:
+            setting = setting[i]
+
+        return setting
+
+    return setting
